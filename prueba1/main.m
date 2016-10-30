efc34ae8b693df8ef8bbbdee8ccbf48207f30839
Luis Esteban <metalerk@mbp-de-macook.lan>
2016-10-29 20:34:30 -0500

    Initial Commit

---
+++ prueba1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prueba1
+//
+//  Created by Luis Esteban on 29/10/16.
+//  Copyright © 2016 Luis Esteban. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
