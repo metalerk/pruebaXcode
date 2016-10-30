eb98d1954240aa778ea177d4a6d98d81543e46e0
Luis Esteban <metalerk@mbp-de-macook.lan>
2016-10-29 21:05:58 -0500

    first commit

--- prueba1/main.m
+++ prueba1/main.m
@@ -5,7 +5,7 @@
 //  Created by Luis Esteban on 29/10/16.
 //  Copyright © 2016 Luis Esteban. All rights reserved.
 //
-
+/*
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
@@ -14,4 +14,13 @@ int main(int argc, const char * argv[]) {
         NSLog(@"Hello, World!");
     }
     return 0;
+}
+*/
+
+#import <stdio.h>
+#import <stdlib.h>
+
+int main(){
+    printf("Hola Mundo !!\n");
+    return EXIT_SUCCESS;
 }
